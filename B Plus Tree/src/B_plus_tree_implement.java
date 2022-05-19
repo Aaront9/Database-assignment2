76a64463882360f9a3134d603a54d9c6d63ab5b6
Bosong Zhang <s3828811@student.rmit.edu.au>
2022-05-19 17:33:30 +1000

    update

--- B Plus Tree/src/B_plus_tree_implement.java
+++ B Plus Tree/src/B_plus_tree_implement.java
@@ -1,4 +1,22 @@
 
-public class B_plus_tree_implement {
 
+public class B_plus_tree_implement {
+        //initialize the root of B+tree
+	    private Object root;
+	    //initialize the value T to ensure the valid size
+	    private final int T;
+	    //initialize the minimal gap between 2 elements
+	    private int MGap;
+	    
+	    public B_plus_tree_implement(int t, int n) {
+	    	this.T = t;
+	    	this.MGap = Integer.MAX_VALUE;
+	    	
+	    }
+	    
+	  //insets the key 'x' into the tree
+		public void insert(int x){
+			int gap= Integer.MAX_VALUE;
+		}
 }
+//still not get satisfied implemtation on B plus tree. keep working on it.
